ff4ca3cfad2a8b7d2c038fee5d56e74618c2ec06
pankaj Yadav <pankajyadav851685@gmail.com>
2025-08-31 00:41:48 +0530

    start learning js

---
+++ ApnaCollege,html,css,js/JavaScript/script.js
@@ -0,0 +1,32 @@
+// console.log("apna college");
+// console.log("Pankaj Yadav");
+// console.log("i love js !");
+
+// name = "Tony Stark";
+// console.log("full name :"+name);
+
+// age = 24;
+// console.log(age);
+
+
+
+// let name = "Tony bhai ";
+// let age = 24;
+// let totalPrice = 2000;
+
+
+const student ={
+    fullName: "Rahul Kumar",
+    age: 20,
+    cgpa: 8.2,
+    isPass: true,
+};
+
+//  student.age = student.age+1;
+//  console.log(student.age);
+// its possible code like thi object update values 
+
+student["cgpa"] = student["cgpa"+1]+1;
+// student.cgpa = student.cgpa+1;
+
+console.log(student.cgpa);
---
+++ ApnaCollege,html,css,js/JavaScript/tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+student.age = student.age+1;
+//  console.log(student.age);
