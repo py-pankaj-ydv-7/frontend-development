f4ad172135b18ba807b282b9b14435cc2c3e1ef4
Pankaj <pankajyadav851685@gmail.com>
2025-09-02 03:06:15 +0530

    arrays in js

---
+++ ApnaCollege,html,css,js/JavaScript/chap4.js
@@ -0,0 +1,17 @@
+////************* Arrays *********/
+
+let marks = [97,82,76, 64,36];
+console.log(marks);         
+console.log(typeof (marks));         
+console.log(marks.length);//    property= value deti hai ,  method = kaam karthai a 
+
+
+//array indices 
+marks[3] = 99 
+console.log(marks[3]);
+
+// looping over an Arrays
+console.log("Looping over an arrays ");
+
+let heroes = ["ironman", "thor","hulk","shaktiman","spiderman","antman"];
+
