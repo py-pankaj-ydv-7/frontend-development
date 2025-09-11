86fff36e48631300eed3b78fe61f222ebe6ddacc
Pankaj Yadav <pankajyadav851685@gmail.com>
2025-09-11 23:34:59 +0530

    classes and objects

---
+++ ApnaCollege,html,css,js/JavaScript/chap11.js
@@ -0,0 +1,15 @@
+ const student ={
+    fullName: "pankaj yadav",
+    marks: 95.4,
+    printMarks: function (){
+        console.log("marks = ", this.marks);
+    },
+ };
+
+ console.log(student.fullName);
+ console.log(student.marks);
+console.log(student); // pura object a jayega .
+ 
+
+//  console.log(student.printMarks());
+ 
