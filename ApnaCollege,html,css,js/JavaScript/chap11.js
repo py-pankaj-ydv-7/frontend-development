fe7cf1a6620c267a117ea8b6f1f367ff46978e50
Pankaj Yadav <pankajyadav851685@gmail.com>
2025-09-12 16:22:18 +0530

    classes and obj

--- ApnaCollege,html,css,js/JavaScript/chap11.js
+++ ApnaCollege,html,css,js/JavaScript/chap11.js
@@ -1,15 +1,120 @@
- const student ={
-    fullName: "pankaj yadav",
-    marks: 95.4,
-    printMarks: function (){
-        console.log("marks = ", this.marks);
-    },
- };
+//  const student ={
+//     fullName: "pankaj yadav",
+//     marks: 95.4,
+//     printMarks: function (){// method
+//         console.log("marks = ", this.marks);
+//     },
+//  };
 
- console.log(student.fullName);
- console.log(student.marks);
-console.log(student); // pura object a jayega .
+// //  console.log(student.fullName);
+// //  console.log(student.marks);
+// // console.log(student); // pura object a jayega .
  
 
-//  console.log(student.printMarks());
+// //  console.log(student.printMarks());
  
+// //********************** ProtoType **************/
+
+// const employee = {
+//     calcTax1(){ // function ko dono tarike se likh sakte hai , 
+//         console.log("Tax rate is 10% ");
+//     },
+//     calcTax2 : function(){
+//         console.log("tax rate is 10%");
+//     }
+// };
+
+// const karanArjun={
+//     salary: 50000,
+// }
+// const karanArjun2={
+//     salary: 50000,
+// }
+// const karanArjun3={
+//     salary: 50000,
+// }
+// const karanArjun4={
+//     salary: 50000,
+// }
+// const karanArjun5={
+//     salary: 50000,
+// }
+
+// karanArjun.__proto__ = employee;// 
+// karanArjun2.__proto__ = employee;//
+// karanArjun3.__proto__ = employee;//
+// karanArjun4.__proto__ = employee;//
+// karanArjun5.__proto__ = employee;//
+
+// //********************** Classes **************/
+// class ToytoCar {
+//     constructor(brand,mileage){
+//         console.log("creating new object");
+//         this.brand = brand;
+//         this.mileage = mileage;
+//     }
+//     start(){
+//         console.log("start");
+//     }
+
+//     stop(){
+//         console.log("stop");
+//     }
+
+//     // setBrand(brand){
+//     //     this.brandName = brand;
+//     // }
+
+// }
+
+// let fortuner = new ToytoCar("fortuner",10); //constructor involk hoga ., fir hame setBrand likhne ki jarurat nai hia hum direct brandname initialization k time pe hi kr rahe hai .
+// console.log(fortuner);
+
+// let lexus = new ToytoCar("lexus");// constructor
+// // fortuner.setBrand("fortuner");
+// // lexus.setBrand("lexus");
+// console.log(lexus);
+
+
+//********************** Inheritance **************/
+
+class Parent {
+    hello(){
+        console.log("hello");
+        
+    }
+}
+
+class child extends Parent{};
+
+let obj = new child();
+
+
+class Person {
+    constructor(name){
+        this.species = "homo sapiens";
+        this.name = name;
+    }
+    eat(){
+        console.log("eat");
+        
+    }
+    // sleep(){
+    //     console.log("sleep");
+        
+    // }
+}
+
+class Engineer extends Person {
+    constructor(name,branch){
+        super(name); // to invoke parent class constructor
+        this.branch = branch;
+        this.name = name
+    }
+    work(){
+        console.log("solve problems, build something");
+    }
+}
+
+let engObj = new Engineer("pankaj","IT");
+
